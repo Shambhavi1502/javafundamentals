6985f207f676013aa6fedf8691b1004e3afb4562
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-05-25 23:17:53 +0530

    Add files via upload

---
+++ revString.java
@@ -0,0 +1,17 @@
+public class revString{
+public static void main(String[] args){
+StringBuilder str = new StringBuilder("HELLO");
+
+for(int i = 0 ; i<str.length()/2 ; i++){
+   int front = i;
+   int back = str.length() -1 -i; //5-1-0=4 h->o
+
+   char frontChar = str.charAt(front);
+   char backChar = str.charAt(back);
+
+   str.setCharAt(front, backChar);
+   str.setCharAt(back, frontChar);
+   System.out.println(str);
+}
+}
+}
