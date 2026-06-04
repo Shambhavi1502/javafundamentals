43e455c17be518370cdfe4ea1eb0eab4c12701e3
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-04 23:47:40 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Functions{
+  public static void PrintMyName(String name) {
+    System.out.println(name);
+    return;
+}
+  public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("enter your name");
+  String name = sc.next();
+  PrintMyName(name);
+}
+}
