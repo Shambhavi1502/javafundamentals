9048ec1efed719b0bc4d80135bc2db3d3d9d3622
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-10 22:50:18 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+public class fibonacci{
+public static void printfib(int a , int b , int n){
+if(n == 0){
+return;
+}
+int c = a + b;
+System.out.print(c + " ");
+printfib(b,c,n-1);
+}
+public static void main(String[] args){
+int a = 0 , b = 1;
+System.out.print(a + " ");
+System.out.print(b + " ");
+int n = 7;
+printfib(a , b , n-2);
+
+}
+}
+
+//C:\Users\Sai\Desktop\cityconnect>java fibonacci.java
+//0 1 1 2 3 5 8
