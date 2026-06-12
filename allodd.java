3e74f71999cbd9532fdaf58e2ef9965b7812c68a
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-12 22:31:02 +0530

    Add files via upload

---
+++ allodd.java
@@ -0,0 +1,17 @@
+//fifth
+public class allodd
+{
+   public static void main(String args[])
+{
+
+int i=1 ;
+while(i<=100)
+{
+   if(i % 2 != 0)
+     System.out.println("odd" + i);
+     i++;
+}
+
+}
+
+}
