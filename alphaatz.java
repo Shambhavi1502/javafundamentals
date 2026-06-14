d0e4d993ae8e4f1b2697bf3ce4c2ffca72a668df
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-14 23:13:26 +0530

    Add files via upload

---
+++ alphaatz.java
@@ -0,0 +1,20 @@
+// third 
+import java.util.*;
+public class alphaatz{
+
+ public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    char ch = 'a';
+   
+    while(ch <= 'z')
+    {
+
+
+    System.out.println(ch);
+    ch++;
+}
+
+
+}
+}
