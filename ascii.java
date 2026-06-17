3bdc29550495ad5f5b50650cbd2068a8174fe3a7
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-18 00:02:15 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ascii{
+    public static void main(String args[])
+{
+
+
+char ch = 'a';
+int I ;
+
+while(ch <= 'z')
+{
+I=(int)ch;
+
+System.out.println("ascii value of" + ch + " is " + I);
+ch++;
+}
+}
+}
