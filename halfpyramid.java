8db6335e5e0291413a1954879f60feefbd0d7df7
Shambhavi Jawalkoti <shambhavijawalkoti0@gmail.com>
2026-06-26 22:03:58 +0530

    Add files via upload

---
+++ halfpyramid.java
@@ -0,0 +1,18 @@
+public class halfpyramid
+{
+ public static void main(String args[])
+ {
+  int n=7;
+  int i , j;
+  for(i=1 ; i<=n ; i++)
+  {
+    for(j=1 ; j<=i ; j++)
+    {
+      System.out.print("*");
+    }
+      System.out.println(" ");
+  }
+ System.out.println();
+ }
+
+}
